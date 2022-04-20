208555782f271a9f5106683b6fe32eec95e6431f
Egor Taran <64767049+egortaran@users.noreply.github.com>
2022-04-20 13:40:52 +0600

    lesson-3

--- lesson-3/script.py
+++ lesson-3/script.py
@@ -1,71 +1,102 @@
-import re
-
 import requests
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
 from transliterate import translit
 
-base_url = 'https://omsk.hh.ru'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
 
-client = MongoClient('127.0.0.1', 27017)
-db = client['hhru']
-vacancies = db.vacancies
+def main():
+    s_vacancy = translit(input('Название вакансии: '), 'ru', reversed=True).lower()
+    s_page = int(input('Количество страниц: '))
 
-s_vacancy = translit(input('Название вакансии: '), 'ru', reversed=True).lower()
-s_page = int(input('Количество страниц: '))
+    data = []
+    for page in range(s_page):
+        if create_html(s_vacancy, s_page):
+            data.append(search_date())
 
-data = []
-for page in range(s_page):
+    for vac in data:
+        write_to_database(vac)
+
+
+def search_date():
+    with open('response.html', 'r', encoding='utf-8') as f:
+        html_file = f.read()
+
+    dom = bs(html_file, 'html.parser')
+
+    vacancies = dom.find_all('div', {'class': 'vacancy-serp-item'})
+    vacancies_list = []
+    for vacancy in vacancies:
+        name = vacancy.find('a', {'class': 'bloko-link'}).getText()
+        link = vacancy.find('a', {'class': 'bloko-link'})['href']
+
+        f_salary = vacancy.find('span', {'class': 'bloko-header-section-3'})
+        salary = find_salary(f_salary)
+
+        vacancies_list.append({'Вакансия': name, 'Зарплата': salary, 'Ссылка': link, 'Сайт': 'hh'})
+
+    return vacancies_list
+
+
+def find_salary(f_salary):
+    salary = {
+        'min': '',
+        'max': '',
+        'currency': ''
+    }
+
+    if f_salary is not None:
+        f_salary = f_salary.getText()
+        fn_salary = f_salary.replace(' ', ' ').replace(' ', '')
+        dash = fn_salary.find('–')
+
+        # Поиск последнего числа в строке
+        last_d = 0
+        for d in range(len(fn_salary)):
+            if fn_salary[d].isdigit():
+                last_d = d
+
+        if dash != -1:  # Пример: 30 000 – 180 000 руб.
+            salary['min'] = int(fn_salary[:dash])
+            salary['max'] = int(fn_salary[dash + 1:last_d + 1])
+        elif fn_salary.find('от') != -1:  # Пример: от 70 000 руб
+            salary['min'] = int(fn_salary[:last_d + 1].replace('от', ''))
+        elif fn_salary.find('до') != -1:  # Пример: до 45 000 руб.
+            salary['max'] = int(fn_salary[:last_d + 1].replace('до', ''))
+        salary['currency'] = fn_salary[last_d + 1:]
+
+    return salary
+
+
+def create_html(s_vacancy, page):
+    base_url = 'https://omsk.hh.ru'
     url = base_url + '/vacancies/' + s_vacancy
 
     page_url = f'?page={page}&hhtmFrom=vacancy_search_catalog'
     if page != 0:
         url + page_url
 
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
     headers = {
         'User-Agent': user_agent,
     }
+
     response = requests.get(url, headers=headers)
+    if response.ok:
+        with open('response.html', 'w', encoding='utf-8') as f:
+            f.write(response.text)
 
-    dom = bs(response.text, 'html.parser')
-    vacancies = dom.find_all('div', {'class': 'vacancy-serp-item'})
+    return response.ok
 
-    for vacancy in vacancies:
-        vacancy_data = {}
-        link_new = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})['href']
-        name_new = vacancy.find('span', {'class': 'resume-search-item__name'}).getText()
-        payment = vacancy.find('span', {'class': 'bloko-header-section-3'})
-        vacancy_company_link = vacancy.find('a', {'class': 'bloko-link bloko-link_kind-tertiary'})['href']
 
-        response2 = requests.get(base_url + vacancy_company_link, headers=headers)
-        dom2 = bs(response2.text, 'html.parser')
-        company_link_new = dom2.find('a', {'rel': 'noopener noreferrer nofollow noindex'})
-        vac_id = re.search(r'\d+', link_new)
+def write_to_database(vac):
+    client = MongoClient('127.0.0.1', 27017)
+    db = client['vacancy_hhru']
+    vacancy = db.vacancy
+    if vacancy.find_one({'link': vac['link']}):
+        pass
+    else:
+        vacancy.insert_one(vac)
 
-        if payment:
-            vacancy_compensation = payment.getText().replace('\u202f', ' ').replace(' ', ' ')
-        else:
-            vacancy_compensation = None
-        if company_link_new:
-            company_link_new = dom2.find('a', {'rel': 'noopener noreferrer nofollow noindex'})['href']
-        else:
-            company_link_new = None
-        vacancy_data['_id'] = vac_id[0]
-        vacancy_data['salary'] = vacancy_compensation
-        vacancy_data['link'] = link_new
-        vacancy_data['name'] = name_new
-        vacancy_data['company_site'] = company_link_new
 
-        try:
-            vacancies.insert_one(vacancy_data)
-        except DuplicateKeyError:
-            print(f"Document with id = {vacancy_data['_id']} already exist")
-
-        vacancy_id = ''
-        vacancy_compensation = ''
-        link = ''
-        name = ''
-        company_link = ''
-        print('--' * page * 10)
+if __name__ == '__main__':
+    main()
